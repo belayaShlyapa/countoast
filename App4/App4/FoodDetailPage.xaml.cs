5ddb74163d3f832b0dda79ba61a31a55187ab7b9
belayaShlyapa <loris.legeay.pro@gmail.com>
2021-03-14 00:27:53 +0100

    Ajout d'un objet directement en base de données

--- App4/App4/FoodDetailPage.xaml.cs
+++ App4/App4/FoodDetailPage.xaml.cs
@@ -26,14 +26,23 @@ namespace CounToast
             BindingContext = ApplicationVM.SelectedFood;
         }
 
-        private void Add_To_Cart(object sender, EventArgs e)
+        private void Add_To_Cart_Button_Clicked(object sender, EventArgs e)
         {
-            ApplicationVM.Factory.AddFood(new Food {
+            Food foodToAdd = new Food {
                 Name = name.Text,
                 Price = Convert.ToDouble(price.Text),
                 Quantity = Convert.ToInt32(quantity.Text),
                 ImageURL = "https://media.istockphoto.com/vectors/hand-drawn-validation-icon-scanned-and-vectorized-brush-drawing-vector-id478369072?b=1&k=6&m=478369072&s=612x612&w=0&h=jAK7PMAvrIpS6eyKv8F6yiWdWYctn8ovM2eNt4Wyoxw="
-        });
+            };
+
+            using (var context = new FoodDbContext(ApplicationVM.databaseFileName))
+            {
+                context.Database.EnsureCreated();
+                context.FoodSet.AddAsync(foodToAdd);
+                context.SaveChangesAsync();
+            }
+
+            // TODO : Bad way to reload page
             Navigation.PopAsync();
             Navigation.PopAsync();
             Navigation.PushAsync(new CountMyCartPage());
