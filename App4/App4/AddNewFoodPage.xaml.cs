849a573843762c250a14d7a8416a868356012dd5
belayaShlyapa <loris.legeay.pro@gmail.com>
2021-03-14 22:17:23 +0100

    Ajout de la fonctionnalité pour ajouter en base de données un aliment personnalisé

--- App4/App4/AddNewFoodPage.xaml.cs
+++ App4/App4/AddNewFoodPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CounToastLibrary;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,39 @@ namespace CounToast
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class AddNewFoodPage : ContentPage
     {
+        private ApplicationViewModel ApplicationVM
+        {
+            get
+            {
+                return (Application.Current as App).ApplicationVM;
+            }
+        }
         public AddNewFoodPage()
         {
             InitializeComponent();
+        }
+
+        private void Add_To_Cart_Button_Clicked(object sender, EventArgs e)
+        {
+            using (var context = new FoodDbContext(ApplicationVM.databaseFileName))
+            {
+                Food foodToAdd = new Food
+                {
+                    Name = name.Text,
+                    Price = Convert.ToDouble(price.Text),
+                    Quantity = Convert.ToInt32(quantity.Text),
+                    ImageURL = "https://media.istockphoto.com/vectors/tableware-line-icon-vector-id1040473414?k=6&m=1040473414&s=612x612&w=0&h=aZcXfva389LegX86VPFVEPVFArMCOkD5hvL-rE-mloQ="
+                };
+
+                context.Database.EnsureCreated();
+                context.FoodSet.AddAsync(foodToAdd);
+                context.SaveChangesAsync();
+            }
+
+            // TODO : Bad way to reload page
+            Navigation.PopAsync();
+            Navigation.PopAsync();
+            Navigation.PushAsync(new CountMyCartPage());
         }
     }
 }
