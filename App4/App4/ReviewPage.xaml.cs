c4c0e6fe68a962690a728ed2dda45303cc295c23
belayaShlyapa <loris.legeay.pro@gmail.com>
2021-03-14 23:39:11 +0100

    Ajout d'un bouton de suppression pour chaque element

--- App4/App4/ReviewPage.xaml.cs
+++ App4/App4/ReviewPage.xaml.cs
@@ -26,5 +26,26 @@ namespace CounToast
             InitializeComponent();
             BindingContext = ApplicationVM;
         }
+
+        private void Delete_Button_Clicked(object sender, EventArgs e)
+        {
+            // Get item id
+            Button button = (Button)sender;
+            Grid listViewItem = (Grid)button.Parent;
+            Label label = (Label)listViewItem.Children[0];
+            int id = Convert.ToInt32(label.Text);
+
+            using (var context = new FoodDbContext(ApplicationVM.databaseFileName))
+            {
+                context.Database.EnsureCreated();
+                context.FoodSet.Remove(ApplicationVM.Foods.Where(f => f.Id == id).First());
+                context.SaveChangesAsync();
+            }
+
+            // TODO : Bad way to reload page
+            Navigation.PopAsync();
+            Navigation.PopAsync();
+            Navigation.PushAsync(new ReviewPage());
+        }
     }
 }
